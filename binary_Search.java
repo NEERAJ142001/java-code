aceb2b0510ae89842dfaea88571320465789c0fe
NEERAJ142001 <126163512+NEERAJ142001@users.noreply.github.com>
2023-02-27 16:21:37 +0530

    Binary search (one by one)

---
+++ binary_Search.java
@@ -0,0 +1,32 @@
+package love_babbar;
+
+import java.util.Scanner;
+
+public class binary_search {
+    private boolean search(int arr[],int size,int element)
+    {
+        for(int i=0;i<size;i++)
+        {
+            if(element==arr[i])
+                return true;
+        }
+        return false;
+    }
+    public static void main(String args[])
+    {
+        binary_search n=new binary_search();
+       Scanner sc=new Scanner(System.in);
+        System.out.println("input size");
+        int x=sc.nextInt();
+        int arr[] = new int[x];
+        System.out.println("input elements");
+        for(int i=0;i<x;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("input element to be searched");
+        int xyz=sc.nextInt();
+        boolean answer=n.search(arr,x,xyz);
+        System.out.println(answer);
+    }
+}
